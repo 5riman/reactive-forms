8e3acf97c3fc69dabdaf780a2c992e0a8a14bd2e
5riman <cheetisriman@gmail.com>
2026-01-14 10:43:26 +0530

    added reactive forms for signup page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule ,FormGroup,FormControl } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('reactive-forms');
+
+  signupform =new FormGroup({
+    name:new FormControl(''),
+    age:new FormControl(''),
+    email:new FormControl(''),
+  })
+
 }
